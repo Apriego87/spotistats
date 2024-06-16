00b7dbdf112b5b9de3ef28843dc4ed9b7df30efb
Apriego87 <antoniopriego22@gmail.com>
2024-06-16 16:03:38 +0200

    a ver ahora

--- src/routes/callback/+page.server.ts
+++ src/routes/callback/+page.server.ts
@@ -6,18 +6,14 @@ export const load: PageServerLoad = async ({ url }) => {
 
     if (code) {
         const tokenResponse = await fetchToken(code)
-        console.log(tokenResponse)
-        return {
-            tokenResponse
-        }
-        /* if (tokenResponse.access_token) {
+        if (tokenResponse.access_token) {
 
             const now = new Date()
             const expiry = new Date(now.getTime() + tokenResponse.expires_in * 1000)
             return {
                 access_token: tokenResponse.access_token, refresh_token: tokenResponse.refresh_token, expires_in: tokenResponse.expires_in, expires: expiry
             }
-        } */
+        }
     } else {
         return {
             message: 'no detecta el code'
