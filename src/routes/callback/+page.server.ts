b17825d415b0b8109df68fbf771332da59fb6929
Apriego87 <antoniopriego22@gmail.com>
2024-06-16 15:58:15 +0200

    return tokenResponse

--- src/routes/callback/+page.server.ts
+++ src/routes/callback/+page.server.ts
@@ -3,24 +3,24 @@ import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async ({ url }) => {
     const code = url.searchParams.get('code')
-    console.log(code)
 
     if (code) {
         const tokenResponse = await fetchToken(code)
-        if (tokenResponse.access_token) {
+        console.log(tokenResponse)
+        return {
+            tokenResponse
+        }
+        /* if (tokenResponse.access_token) {
 
             const now = new Date()
             const expiry = new Date(now.getTime() + tokenResponse.expires_in * 1000)
             return {
                 access_token: tokenResponse.access_token, refresh_token: tokenResponse.refresh_token, expires_in: tokenResponse.expires_in, expires: expiry
             }
-        }
+        } */
     } else {
         return {
             message: 'no detecta el code'
         }
-    }
-    return {
-        code
     }
 }
