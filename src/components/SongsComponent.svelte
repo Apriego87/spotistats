1c2ef2e67f3aa42edcb959c74959ea294b2fccee
apriego87 <antonio.priego@m.e-tecnia.es>
2025-02-07 13:42:39 +0100

    canciones no encontradas

--- src/components/SongsComponent.svelte
+++ src/components/SongsComponent.svelte
@@ -43,7 +43,9 @@
 </script>
 
 <Card.Root
-	class="max-w-screen isolate flex h-[67vh] w-full flex-col overflow-hidden rounded-xl border bg-white/20 text-white shadow-lg ring-1 ring-black/5 "
+	class={`max-w-screen isolate flex w-full flex-col overflow-hidden rounded-xl border bg-white/20 text-white shadow-lg ring-1 ring-black/5 ${
+		filtered_songs && filtered_songs.length === 0 ? 'h-auto' : 'h-[67vh]'
+	}`}
 >
 	<Card.Header class="flex-shrink-0">
 		<Card.Title>
@@ -69,28 +71,86 @@
 			</Tabs.List>
 			<Tabs.Content value="top">
 				<div class="no-scrollbar">
-					{#each filtered_songs as song}
+					{#if filtered_songs.length == 0 || filtered_songs == null || filtered_songs == ''}
+						<div
+							class="flex-column m-2 flex h-full w-full items-center justify-center p-2 text-center"
+						>
+							<b>No se han encontrado resultados</b>
+						</div>
+					{:else}
+						{#each filtered_songs as song}
+							<div class="w-fit">
+								<a
+									href={song.external_urls.spotify}
+									target="_blank"
+									class="flex w-full flex-row items-center gap-4 p-2"
+								>
+									<p class="font-bold">{song.original_index}</p>
+									<img
+										class="size-12 rounded-full object-cover"
+										src={song.album.images[0].url}
+										alt={song.name}
+									/>
+									<div>
+										<p class="font-bold">{song.name}</p>
+										<div class="flex flex-row flex-wrap">
+											{#each song.artists as artist, index}
+												<p class="text-sm">
+													{artist.name}{index !== song.artists.length - 1 ? ', ' : ''}
+												</p>
+											{/each}
+										</div>
+									</div>
+								</a>
+							</div>
+							{#if song.original_index < songs.length}
+								<Separator class="my-2" />
+							{/if}
+						{/each}
+					{/if}
+				</div>
+			</Tabs.Content>
+			<Tabs.Content value="recent">
+				{#if filtered_songs.length == 0 || filtered_songs == null || filtered_songs == ''}
+					<div
+						class="flex-column m-2 flex h-full w-full items-center justify-center p-2 text-center"
+					>
+						<b>No se han encontrado resultados</b>
+					</div>
+				{:else}
+					{#each filtered_recents as song}
 						<div class="w-fit">
 							<a
-								href={song.external_urls.spotify}
+								href={song.track.external_urls.spotify}
 								target="_blank"
 								class="flex w-full flex-row items-center gap-4 p-2"
 							>
-								<p class="font-bold">{song.original_index}</p>
+								<p>{song.original_index}</p>
 								<img
 									class="size-12 rounded-full object-cover"
-									src={song.album.images[0].url}
+									src={song.track.album.images[0].url}
 									alt={song.name}
 								/>
 								<div>
-									<p class="font-bold">{song.name}</p>
+									<p class="font-bold">{song.track.name}</p>
+
 									<div class="flex flex-row flex-wrap">
-										{#each song.artists as artist, index}
+										{#each song.track.artists as artist, index}
 											<p class="text-sm">
-												{artist.name}{index !== song.artists.length - 1 ? ', ' : ''}
+												{artist.name}{index !== song.track.artists.length - 1 ? ', ' : ''}
 											</p>
 										{/each}
 									</div>
+									<p class="text-xs">
+										{(() => {
+											const date = new Date(song.played_at)
+											const day = String(date.getDate()).padStart(2, '0')
+											const month = String(date.getMonth() + 1).padStart(2, '0')
+											const hours = String(date.getHours()).padStart(2, '0') // Local hours
+											const minutes = String(date.getMinutes()).padStart(2, '0')
+											return `${day}/${month}, ${hours}:${minutes}`
+										})()}
+									</p>
 								</div>
 							</a>
 						</div>
@@ -98,49 +158,7 @@
 							<Separator class="my-2" />
 						{/if}
 					{/each}
-				</div>
-			</Tabs.Content>
-			<Tabs.Content value="recent">
-				{#each filtered_recents as song}
-					<div class="w-fit">
-						<a
-							href={song.track.external_urls.spotify}
-							target="_blank"
-							class="flex w-full flex-row items-center gap-4 p-2"
-						>
-							<p>{song.original_index}</p>
-							<img
-								class="size-12 rounded-full object-cover"
-								src={song.track.album.images[0].url}
-								alt={song.name}
-							/>
-							<div>
-								<p class="font-bold">{song.track.name}</p>
-
-								<div class="flex flex-row flex-wrap">
-									{#each song.track.artists as artist, index}
-										<p class="text-sm">
-											{artist.name}{index !== song.track.artists.length - 1 ? ', ' : ''}
-										</p>
-									{/each}
-								</div>
-								<p class="text-xs">
-									{(() => {
-									  const date = new Date(song.played_at);
-									  const day = String(date.getDate()).padStart(2, '0');
-									  const month = String(date.getMonth() + 1).padStart(2, '0');
-									  const hours = String(date.getHours()).padStart(2, '0');  // Local hours
-									  const minutes = String(date.getMinutes()).padStart(2, '0');
-									  return `${day}/${month}, ${hours}:${minutes}`;
-									})()}
-								  </p>
-							</div>
-						</a>
-					</div>
-					{#if song.original_index < songs.length}
-						<Separator class="my-2" />
-					{/if}
-				{/each}
+				{/if}
 			</Tabs.Content>
 		</Tabs.Root>
 	</Card.Content>
